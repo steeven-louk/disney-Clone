a5ea200ee89c3a87570b488258975d3502ba0bf7
steeven-louk <steevenlouk.sl@gmail.com>
2021-12-14 16:39:27 +0100

    finalisation du projet

--- app.js
+++ app.js
@@ -49,5 +49,40 @@ for(let i=0; i<3; i++){
 }
 
 setInterval(() =>{
-  //  createSlide();
+    createSlide();
 } ,3000);
+
+//////video cards
+
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item => {
+    item.addEventListener('mouseover', () =>{
+        let video = item.children[1];
+        video.play();
+    })
+
+    item.addEventListener('mouseleave', () =>{
+        let video = item.children[1];
+        video.pause();
+    })
+})
+
+//card sliders
+
+let cardContainers =[...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item, i)=>{
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width
+
+    nxtBtns[i].addEventListener('click', ()=> {
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    nxtBtns[i].addEventListener('click', ()=> {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
